b3dd0c399238aa853afbde8bb81a1fc31f576eb5
Linnea <heartbrrrrr@protonmail.com>
2023-05-15 11:38:33 +0200

    Sliding window sha and bar graph instead of hist

--- graph.py
+++ graph.py
@@ -1,26 +1,41 @@
 import matplotlib.pyplot as plt
 import math
+import numpy as np
 
 
 numbers = []
             # numbers.append(1.0/(float(number)/5000000.0))
 
 
-with open('mod50.txt','r') as f:
-    for row in f:
+with open('numbers.txt','r') as f:
+    file = f.readlines()
+    for row in file:
         row = int(row[:-1])
-        numbers.append(row)
-        # i = 0b10000000
-        # while(i):
-        #     number = row&i
-        #     i = i >> 1
+        # numbers.append(row)
+        i = 0b1000000000000000
+        while(i):
+            number = row&i
+            i = i >> 1
 
-        #     if number:
-        #         numbers.append(int(math.log2(number))+1)
-                
+            if number:
+                numbers.append((int(math.log2(number))+1))
 
 
-numbers = numbers[:-1]
+# numbers = numbers[:-1]
 
-plt.hist(numbers, bins=50)
+labels, counts = np.unique(numbers, return_counts=True)
+labels = np.insert(labels[:-8], 1, labels[-8:])
+counts = np.insert(counts[:-8], 1, counts[-8:])
+counts = [number/len(file)*100 for number in counts]
+
+
+plt.bar(labels, counts)
+plt.ylabel("Procent")
+plt.xlabel("bit position")
+
+# Get your current y-ticks (loc is an array of your current y-tick elements)
+# loc, labels = plt.yticks()
+
+# This sets your y-ticks to the specified range at whole number intervals
+# plt.yticks(np.arange(0, max(loc), step=1))
 plt.show()
--- serialread.py
+++ serialread.py
@@ -19,41 +19,17 @@ while 1:
         # Print the contents of the serial data
         try:
             number = int(serialString.decode("Ascii")[:-1])
-
-            # with open("and_255.txt", "a") as f:
-            #     f.write(str(number&255)+"\n")
-            
-            # with open("mod10.txt", "a") as f:
-            #     f.write(str(number%10)+"\n")
-
-            # with open("mod100.txt", "a") as f:
-            #     f.write(str(number%100)+"\n")
-
-            # with open("mod50.txt", "a") as f:
-            #     f.write(str(number%50)+"\n")
              
-
+            #SHA256 sliding window
             mybytes.extend((number&255).to_bytes(1, 'little', signed=False))
-            if len(mybytes) >= 256:
-                with open("sha.bin", "a+b") as f:
-                    print(mybytes.hex())
-                    f.write(hashlib.sha256(mybytes).digest())
-                    mybytes = bytearray()
+            if len(mybytes) >= 32:
+                with open("sliding_window_second_iteration.bin", "a+b") as f:
+                    sha = hashlib.sha256(mybytes).digest()
+                    f.write(sha)
+                    print(sha.hex())
+                    mybytes = mybytes[-31:]
 
-            # bits = bitarray(endian='little')
-            # bits.frombytes(number.to_bytes(16, 'little', signed=False))
-            # after_extractor = bitarray()
-            # while bits:
-            #     bit1 = bits.pop()
-            #     bit2 = bits.pop()
 
-            #     if bit1 != bit2:
-            #         after_extractor.append(bit1)
-            # with open("von_neuman.bin", "a+b") as f:
-            #     f.write(after_extractor)
-
-            # with open("numbers.txt", "+a") as f:
-            #     f.write(serialString.decode("Ascii")[:-1])
-            print(number)
+            # print(number)
         except:
             pass
